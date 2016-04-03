53e054793eb64ec5b2d5ef4c05eb771567746e59
Ion <ionciubotaru@users.noreply.github.com>
2016-04-03 12:34:47 +0300

    First commit

---
+++ data/ota.bat
@@ -0,0 +1,13 @@
+@echo off
+@echo Incep
+:start
+IF NOT EXIST %1 GOTO :end
+@echo Transfer
+d:\bat\curl -F "image=@%1" %2/update
+del %1
+@echo .
+goto :start
+:end
+ping 1.1.1.1 -n 1 -w 1000 > nul
+goto :start
+
---
+++ data/upload.bat
@@ -0,0 +1,42 @@
+@echo off 
+curl %1/format
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl -F "file=@jquery.mobile-1.4.5.min.css.gz" %1/edit
+@echo.
+@echo jquery.mobile-1.4.5.min.css.gz
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl -F "file=@jquery-1.11.1.min.js.gz" %1/edit
+@echo jquery-1.11.1.min.js.gz
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl -F "file=@jquery.mobile-1.4.5.min.js.gz" %1/edit
+@echo jquery.mobile-1.4.5.min.js.gz
+rem ping 1.1.1.1 -n 1 -w 1000 > nul
+rem curl -F "file=@jquery.mobile-1.4.5.min.map.gz" %1/edit
+rem @echo jquery.mobile-1.4.5.min.map.gz
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl -F "file=@edit.htm.gz" %1/edit
+@echo edit
+rem ping 1.1.1.1 -n 1 -w 1000 > nul
+rem curl -F "file=@favicon.ico" %1/edit
+rem @echo favico
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl -F "file=@graphs.js.gz" %1/edit
+@echo graphs
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl -F "file=@ajax-loader.gif" %1/edit
+@echo ajax-loader
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl -F "file=@index.htm" %1/edit
+@echo index
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl -F "file=@redirect.htm" %1/edit
+@echo redirect
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl -F "file=@gpio.txt" %1/edit
+@echo gpio
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl -F "file=@ssdp.txt" %1/edit
+@echo ssdp
+ping 1.1.1.1 -n 1 -w 1000 > nul
+curl %1/reset
+pause

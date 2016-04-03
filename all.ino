77870ec9c4a0d9c929b752d6efad77de31e0ad5a
Ion <ionciubotaru@users.noreply.github.com>
2016-04-03 13:22:37 +0300

    Update all.ino

--- all.ino
+++ all.ino
@@ -1,6 +1,3 @@
-//WiFi.forceSleepBegin();
-//WiFi.forceSleepWake();
-
 #define ErrorLed 13
 
 #include <ESP8266WiFi.h>
@@ -24,9 +21,6 @@ DNSServer dnsServer;
 boolean isAP=false ;
 String ssidN="",ssidP="",session="",uName="",uPass="",apName="",apPass="";
 unsigned long time1=0,time2=0;
-
-#include <OneWire.h>
-OneWire  ds(14); 
 
 void setup() {
   Serial.begin(115200);

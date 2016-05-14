52b4db2e426aba09fd7c0b3f18a5c57134ce51c0
Ion <ionciubotaru@users.noreply.github.com>
2016-05-14 13:20:02 +0300

    add mqtt support

--- ESP8266-Easy-config.ino
+++ ESP8266-Easy-config.ino
@@ -1,3 +1,11 @@
+
+/* 
+ *  Tested with Arduino IDE 1.6.8
+ * Please install the following libraries:
+ * https://github.com/bblanchon/ArduinoJson
+ * https://github.com/Imroy/pubsubclient
+ */
+
 #define ErrorLed 13
 
 #include <ESP8266WiFi.h>
@@ -8,19 +16,25 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
 #include <DNSServer.h>
-#include "FS.h"
 #include <Ticker.h>
 #include <EEPROM.h>
 #include <ArduinoJson.h>
+#include "FS.h"
+#include <PubSubClient.h>
 
+WiFiClient wclient;
+PubSubClient mqttClient(wclient);
+File fsUploadFile;
 Ticker wifiErrTick;
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
-File fsUploadFile;
 DNSServer dnsServer;
-boolean isAP=false ;
-String ssidN="",ssidP="",session="",uName="",uPass="",apName="",apPass="";
+
+boolean isAP=false,hasCustomPage=false,hasMqtt=false,subscribedMqtt=false;
+String ssidN="",ssidP="",session="",uName="",uPass="",apName="",apPass="",chipId="",mqttBase="easyESP";
 unsigned long time1=0,time2=0;
+const char* mqttUser = "";
+const char* mqttPass = "";
 
 void setup() {
   Serial.begin(115200);
@@ -32,6 +46,8 @@ void setup() {
   startWifi();
   configServer();
   pinMode(0, INPUT);
+  mqttSetup();
+  customSetup();
 }
 
 void loop() {
@@ -39,9 +55,10 @@ void loop() {
   checkClean();
   if (isAP) {
     dnsServer.processNextRequest();
-    if (millis()>time1+300000) reset();
+    if (millis()>time1+300000) rst();
   } else {
-    if (WiFi.status() != WL_CONNECTED) reset();
+    if (WiFi.status() != WL_CONNECTED) rst();
+    mqttLoop();
   }
+  customLoop();
 }
-
